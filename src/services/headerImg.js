0f5f0651078c790982c791062918022d28fd40a1
0abilash <mailtoabilashy@gmail.com>
2026-08-04 21:25:49 +0530

    Minnor changes

--- src/services/headerImg.js
+++ src/services/headerImg.js
@@ -1,4 +1,4 @@
-const headerModules = import.meta.glob('../images/profile-header.*', { eager: true })
+const headerModules = import.meta.glob('../images/pimage.*', { eager: true })
 const headerImg = Object.values(headerModules)[0]?.default || null
 
 export default headerImg
